08110bffe99b5dd605693efa96077255f38b6a6c
Elibrer <eli.brer@gmail.com>
2023-06-26 12:25:14 +0930

    Filled out package.json files. Filled out webpack.config.js file. Updated src-sw.js file. Updated install.js file. Updated database.js file (not working yet)

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Updating database...');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = await store.put(content);
+  const result = await request;
+  console.log('Database updated', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Getting data from database...');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = await store.getAll();
+  const result = await request;
+  console.log('Data retrieved', result);
+  return result;
+};
 
 initdb();
