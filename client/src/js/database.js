2c8157c7ce011f5e8f7c6032138874e5d2c19ae6
Elibrer <eli.brer@gmail.com>
2023-06-27 22:04:11 +0930

    fixed install button hiding after pressing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,11 +15,9 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Updating database...');
-
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  
   try {
     const request = store.put({ id: 1,  value: content });
     const result = await request;
@@ -37,13 +35,11 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
-
   if (result) {
     console.log('Data retrieved', result);
     return result.value;
   }
   return false;
-
 };
 
 initdb();
